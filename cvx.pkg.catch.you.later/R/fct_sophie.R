970693729b5c1d5a68a2cb1b19275fd0491680a2
Sophie Apolozan <sophie.apolozan@unknown.home>
2023-10-31 21:19:30 +0000

    sophie

---
+++ cvx.pkg.catch.you.later/R/fct_sophie.R
@@ -0,0 +1,3 @@
+sophie <- function(){
+  "James, thanks for bringing me on board, it’s been a pleasure working for you for the past couple of months. Wishing you all the best for this big next chapter of your life!"
+}
