0ff81c5ae0da06b4223f4943e7dc39bad618c199
Assa Camara <assa.camara@CVX-0724.convexin.local>
2023-11-01 15:26:58 +0000

    added message Assa

---
+++ cvx.pkg.catch.you.later/R/fct_assa.R
@@ -0,0 +1,3 @@
+assa <- function() {
+  "Dear James, in the past six months, you have taught me more than any of my previous bosses/mentors. You have been the best and most understanding manager I could have ever asked for. Enjoy the adventures ahead, and I wish you the best of luck :)"
+}
